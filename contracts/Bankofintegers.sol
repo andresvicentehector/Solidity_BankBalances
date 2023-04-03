d83e568ce963a1837f520145ff298954e4bb79cf
andresvicentehector <andresvicentehector@gmail.com>
2023-04-03 14:58:39 +0200

    ejercicio banco

--- contracts/Bankofintegers.sol
+++ contracts/Bankofintegers.sol
@@ -13,11 +13,12 @@ contract BankOfIntegers{
 
 mapping(address=>uint) private balance;
 
+event Transfer(address _from ,address _to, uint _amount);
 
 
-function addBalance(uint amount ) external returns (uint){
+function addBalance(uint _amount ) external returns (uint){
 
-balance[msg.sender] =balance[msg.sender] + amount;
+balance[msg.sender] =balance[msg.sender] + _amount;
 
 return getBalance();
 
@@ -29,11 +30,28 @@ return balance[msg.sender];
 
 }
 
-function transferBalance(address receiber, uint amount ) public  {
+function _transfer(address _sender, address _receiber, uint _amount ) private  {
 
-balance[receiber]= balance[receiber]+amount;
+if(balance[_sender]>=_amount){
+    
+    balance[_sender]-=_amount;
+    balance[_receiber]= balance[_receiber]+_amount;
+    emit Transfer( _sender, _receiber, _amount);
+    }
+
+
+
+
 
 }
+
+function transfer(address _receiber, uint _amount ) public  returns (uint) {
+
+_transfer(msg.sender, _receiber, _amount);
+return balance[msg.sender];
+
+}
+
 
 
 }
